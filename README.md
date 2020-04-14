d7efe3c9fa27f2bca0b0a9b7b4ffbc3d307dfc9e
Hao <hao@sporule.com>
2020-04-14 17:59:56 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# markdown-sitemap-generator
+# Markdown RSS Generator Plugin
+
+> Generate RSS from markdown
+
+## Installation
+
+Node.js
+
+```bash
+npm install markdown-rss-generator-webpack-plugin
+```
+
+## Usage Example (ES6)
+
+In your Webpack Config file
+
+```javascript
+const MarkdownRSSGeneratorPlugin = require("markdown-rss-generator-webpack-plugin").default;
+
+//define the options
+const option = {
+                title: "Sporule",
+                outputPath: "rss.xml", //rss file output path
+                description: "Sporule is a micro blog site",
+                link: "https://www.sporule.com",
+                language: "en",
+                image: "https://i.imgur.com/vfh3Une.png",
+                favicon: "https://i.imgur.com/vfh3Une.png",
+                copyright: "All rights reserved 2019, Sporule",
+                updated: new Date(), //updated date
+                generator: "Sporule",
+                author: {
+                    name: "Sporule",
+                    email: "example@example.com",
+                    link: "https://www.sporule.com"
+                },
+                route: "/items",
+                //this is the route for the markdown link. If the path of the markdown files are /posts/sporule.md, and the page link for rendered markdown is /items/sporule, then the route is /items
+                useAtom:true //it will return atom xml if it is true, otherwise it will return rss version 2 xml.
+    }
+
+//In the plugin section
+
+ plugins: [
+  new MarkdownRSSGeneratorPlugin(option)
+ ]
+
+```
---
+++ src/index.js
@@ -0,0 +1,83 @@
+import { RawSource } from "webpack-sources";
+import MarkdownHandler from 'markdown-handler';
+
+
+export default class MarkdownSiteMapGeneratorPlugin {
+    constructor(options = {}) {
+        this.options = Object.assign(
+            {
+                host: "https://www.sporule.com",
+                links: [],
+                route: "/items",
+                outputPath:"sitemap.txt"
+            },
+            options
+        );
+    }
+
+    sortPost = (mds, isDesc = true) => {
+        mds = mds.sort((a, b) => {
+            let dateA = new Date(a.metas.date);
+            let dateB = new Date(b.metas.date);
+            return isDesc ? dateB - dateA : dateA - dateB;
+        })
+        return mds;
+    }
+
+    removeFuturePosts = (mds) => {
+        //remove future and posts with no date
+        mds = mds.filter(post => {
+            if (post.metas.date && post.metas.date != "null") {
+                return new Date(post.metas.date) <= new Date();
+            }
+            return false;
+        })
+        return mds;
+    }
+
+    getCategories = (posts) => {
+        let categories = [];
+        posts.forEach(item => {
+            item.metas.categories.forEach(o => {
+                if (!categories.includes(o)) {
+                    categories.push(o);
+                }
+            })
+        });
+        return categories;
+    }
+
+    apply(compiler) {
+        // Specify the event hook to attach to
+        compiler.hooks.emit.tapAsync(
+            'RSSGeneratorPlugin',
+            (compilation, callback) => {
+                let mds = [];
+                let mdHandler = new MarkdownHandler();
+                let regex = /.*\.md$/gm;
+                for (let path in compilation.assets) {
+                    if (path.search(regex) >= 0) {
+                        let md = compilation.assets[path].source().toString();
+                        mds.push(mdHandler.parseContent(path, md));
+                    }
+                }
+                mds = this.removeFuturePosts(mds);
+                mds = this.sortPost(mds, true);
+                let all_links = [];
+                let categories = this.getCategories(mds);
+                categories.forEach(cat => {
+                    all_links.push(this.options.host + "/categories/" + cat);
+                })
+                mds.forEach(md => {
+                    let path = md.path.split(".")[0].replace("posts", this.options.route);
+                    all_links.push(this.options.host + path);
+                })
+                all_links = [this.options.host, ...this.options.links, ...all_links]
+                compilation.assets[this.options.outputPath] = new RawSource(all_links.join("\n"));
+                callback();
+            }
+        );
+
+
+    }
+}
